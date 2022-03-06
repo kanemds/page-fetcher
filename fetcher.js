4f728a5d10643b3c753b1d8d291d748882ecccd9
Kanem <kanemdeng@email.com>
2022-03-06 08:30:45 -0800

    installed request

--- fetcher.js
+++ fetcher.js
@@ -1,8 +1,19 @@
 const input = process.argv.slice(2);
-
+// console.log(input);
+const url = input.toString();
+// console.log(url);
+const request = require(`request`);
 const fs = require(`fs`);
 
-const dl = fs.writeFile(`./new`, input, (error) => {
-  if (error) throw error;
-  console.log(`Downloaded and saved 3261 bytes to ./index.html`);
-});
+
+const fetcher = () => {
+  request(url, (error, response, body) => {
+    // const data = JSON.parse(body);
+    fs.writeFile(`./new1`, url, (error) => {
+      if (error) throw error;
+      console.log(`Downloaded and saved 3261 bytes to ./index.html`);
+    });
+  });
+};
+
+fetcher();
